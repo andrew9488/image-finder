f86a2db885698831d7c52939965b7a74f0574a26
andrew9488 <pashkevichandrew@gmail.com>
2021-07-03 23:52:56 +0300

    feat: create 404 page

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import {Main} from "../../features/Main/Main";
 import {Bookmarks} from "../../features/Bookmarks/Bookmarks";
+import {Error404} from "../Error404/Error404";
 
 export const PATH = {
     main: "/",
-    bookmarks: "/bookmarks"
+    bookmarks: "/bookmarks",
+    error: "/404"
 }
 
 export const Routes: React.FC = React.memo(() => {
@@ -13,6 +15,8 @@ export const Routes: React.FC = React.memo(() => {
         <Switch>
             <Route exact path={PATH.main} render={() => <Main/>}/>
             <Route path={PATH.bookmarks} render={() => <Bookmarks/>}/>
+            <Route path={PATH.error} render={() => <Error404/>}/>
+            <Redirect from={"*"} to={PATH.error}/>
         </Switch>
     )
 })
